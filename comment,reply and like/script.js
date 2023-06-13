8712fbfc2b7fc89db6b7c53116e81bff44a279f1
Balaramtech <65490609+Balaramtech@users.noreply.github.com>
2023-06-13 20:29:12 +0530

    script.js

--- comment,reply and like/script.js
+++ comment,reply and like/script.js
@@ -1,61 +1,111 @@
-let commentContainer = document.getElementById("comment-container");
-let replyContainer = document.getElementById("reply-container");
-
-/* Create a input box for comment and reply */
-function createInputBox() {
-    let div = document.createElement("div");
-    div.setAttribute("class", "comment-details");
-
-    div.innerHTML += `
-            <input type="text" placeholder="Add Your Reply" class="input">
-            <button class="btn submit">Post</button>`;
-
-    return div;
+/* Function to create a new reply element */
+function createReplyElement(content) {
+    const replyElement = document.createElement("div");
+    replyElement.classList.add("reply");
+    replyElement.innerHTML = `
+    <p>${content}</p>
+    <button class="like">Like</button>
+    <span class="likes">0</span>
+    <button class="reply">Reply</button>
+    <button class="delete">Delete</button>
+    <div class="reply-box" style="display: none;">
+      <input placeholder="Enter your reply"></input>
+      <button class="post-reply">Post</button>
+    </div>
+    <div class="replies"></div>
+  `;
+    return replyElement;
 }
 
-/* To Show the results */
-function addReply(text) {
-    let div = document.createElement("div");
-    div.setAttribute("class", "all-comment");
-    div.innerHTML += `
-        <div class="cardcomment" id="forremove">
-            <span class="text">${text}</span>
-            <span id="reply" class="rreply">Add Reply</span>
-            <button class="remove" onclick="myFunction()">Remove Comment</button>
-        </div>`;
-    return div;
+/* Post Img Like Count */
+var like = document.getElementById("likeme"),
+    count = 0;
+like.onclick = function () {
+    count += 1;
+    like.innerHTML = "Likes: " + count;
+};
+
+/* Toggle(display none and block) for main comment input */ 
+function toggleContent() {
+    var content = document.getElementById("comments");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
 }
 
-/* Click Event */
-commentContainer.addEventListener("click", function (e) {
-    let replyClicked = e.target.classList.contains("reply");
-    let rreplyClicked = e.target.classList.contains("rreply");
-    let submitClicked = e.target.classList.contains("submit");
-    let closestCard = e.target.closest(".all-comment")
 
-    if (replyClicked) {
-        // add input box
-        closestCard.appendChild(createInputBox());
-    }
+/* Function to add a new reply to the thread */
+function addReply(content, parentReply = null) {
+    const replyElement = createReplyElement(content);
+    const replyBox = replyElement.querySelector(".reply-box");
+    const postReplyButton = replyElement.querySelector(".post-reply");
 
-    if (rreplyClicked) {
-        // add input box
-        closestCard.appendChild(createInputBox());
-        
-    }
-
-    if (submitClicked) {
-        // add reply box
-        const commentDetails = e.target.closest(".comment-details");
-        if (commentDetails.children[0].value) {
-            closestCard.appendChild(addReply(commentDetails.children[0].value));
-            commentDetails.remove();
+    /* Function to handle posting a reply */
+    function postReply() {
+        const replyContent = replyBox.querySelector("input").value;
+        if (replyContent.trim() !== "") {
+            addReply(replyContent, replyElement);
+            replyBox.style.display = "none";
         }
     }
-})
 
-/* To Remove the Added comments or Replys */
-function myFunction() {
-    let x = document.getElementById("forremove");
-    x.remove(x.selectedIndex);
+    /* Add event listener for post reply button */
+    postReplyButton.addEventListener("click", postReply);
+
+    if (parentReply) {
+        const repliesContainer = parentReply.querySelector(".replies");
+        repliesContainer.appendChild(replyElement);
+    } else {
+        const thread = document.getElementById("comments");
+        thread.appendChild(replyElement);
+    }
+
+    /* Function to handle deleting a reply */
+    function deleteReply() {
+        replyElement.remove();
+    }
+
+    /* Add event listener for delete button */
+    const deleteButton = replyElement.querySelector(".delete");
+    deleteButton.addEventListener("click", deleteReply);
+
+    /* Function to handle liking a reply */
+    function likeReply() {
+        const likesElement = replyElement.querySelector(".likes");
+        let likes = parseInt(likesElement.innerText);
+        likes++;
+        likesElement.innerText = likes.toString();
+    }
+
+    /* Add event listener for like button */
+    const likeButton = replyElement.querySelector(".like");
+    likeButton.addEventListener("click", likeReply);
+
+    /* Function to handle toggling reply input visibility */
+    function toggleReplyBox() {
+        if (replyBox.style.display === "none") {
+            replyBox.style.display = "block";
+        } else {
+            replyBox.style.display = "none";
+        }
+    }
+
+    /*Add event listener for reply button */
+    const replyButton = replyElement.querySelector(".reply");
+    replyButton.addEventListener("click", toggleReplyBox);
 }
+
+/* Function to handle posting the main reply */
+function postMainReply() {
+    const mainReplyContent = document.getElementById("main-reply").value;
+    if (mainReplyContent.trim() !== "") {
+        addReply(mainReplyContent);
+        document.getElementById("main-reply").value = "";
+    }
+}
+
+/* Add event listener for post main reply button */
+const postMainReplyButton = document.getElementById("post-main-reply");
+postMainReplyButton.addEventListener("click", postMainReply);

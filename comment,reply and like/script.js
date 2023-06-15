ecc03a29fa935899153f99c6abbf5f6c7973b53a
Akula Balram <65490609+Balaramtech@users.noreply.github.com>
2023-06-15 21:51:55 +0530

    script.js

--- comment,reply and like/script.js
+++ comment,reply and like/script.js
@@ -1,111 +1,116 @@
-/* Toggle(display none and block) for main comment input */
+// Toggle (display none and block) for main comment input
 function toggleContent() {
     var content = document.getElementById("comments");
     if (content.style.display === "none") {
-        content.style.display = "block";
+      content.style.display = "block";
     } else {
-        content.style.display = "none";
+      content.style.display = "none";
     }
-}
-
-/* Post Img Like Count */
-var like = document.getElementById("likeme"),
-    count = 0;
-like.onclick = function () {
+  }
+  
+  // Post Img Like Count
+  var like = document.getElementById("likeme");
+  var count = 0;
+  like.onclick = function () {
     count += 1;
     like.innerHTML = "Likes: " + count;
-};
-
-/* Function to handle posting the main reply */
-function postMainReply() {
+  };
+  
+  // Function to handle posting the main reply
+  function postMainReply() {
     const mainReplyContent = document.getElementById("main-reply").value;
     if (mainReplyContent.trim() !== "") {
-        addReply(mainReplyContent);
-        document.getElementById("main-reply").value = "";
+      addReply(mainReplyContent);
+      document.getElementById("main-reply").value = "";
     }
-}
-
-/* Add event listener for post main reply button */
-const postMainReplyButton = document.getElementById("post-main-reply");
-postMainReplyButton.addEventListener("click", postMainReply);
-
-/* Function to create a new reply element */
-function createReplyElement(content) {
+  }
+  
+  // Add event listener for post main reply button
+  const postMainReplyButton = document.getElementById("post-main-reply");
+  postMainReplyButton.addEventListener("click", postMainReply);
+  
+  // Function to create a new reply element
+  function createReplyElement(content) {
     const replyElement = document.createElement("div");
     replyElement.classList.add("reply");
     replyElement.innerHTML = `
-    <p>${content}</p>
-    <button class="like">Like</button>
-    <span class="likes">0</span>
-    <button class="reply">Reply</button>
-    <button class="delete">Delete</button>
-    <div class="reply-box" style="display: none;">
-      <input placeholder="Enter your reply"></input>
-      <button class="post-reply">Post</button>
-    </div>  
-    <div class="replies"></div>
-  `;
+      <p>${content}</p>
+      <button class="like">Like</button>
+      <span class="likes">0</span>
+      <button class="reply">Reply</button>
+      <button class="delete">Delete</button>
+      <div class="reply-box" style="display: none;">
+        <input class="reply-input" placeholder="Enter your reply"></input>
+        <button class="post-reply">Post</button>
+      </div>
+      <div class="replies"></div>
+    `;
+  
+    const replyButton = replyElement.querySelector(".reply");
+    const replyBox = replyElement.querySelector(".reply-box");
+  
+    // Function to handle toggling reply input visibility
+    function toggleReplyBox() {
+      if (replyBox.style.display === "none") {
+        replyBox.style.display = "block";
+        replyBox.querySelector(".reply-input").focus(); // Set focus on the input field
+      } else {
+        replyBox.style.display = "none";
+      }
+    }
+  
+    // Add event listener for reply button
+    replyButton.addEventListener("click", toggleReplyBox);
+  
     return replyElement;
-}
-
-
-/* Function to add a new reply to the thread */
-function addReply(content, parentReply = null) {
+  }
+  
+  // Function to add a new reply to the thread
+  function addReply(content, parentReply = null) {
     const replyElement = createReplyElement(content);
     const replyBox = replyElement.querySelector(".reply-box");
     const postReplyButton = replyElement.querySelector(".post-reply");
-
-    /* Function to handle posting a reply */
+  
+    // Function to handle posting a reply
     function postReply() {
-        const replyContent = replyBox.querySelector("input").value;
-        if (replyContent.trim() !== "") {
-            addReply(replyContent, replyElement);
-            replyBox.style.display = "none";
-        }
+      const replyContent = replyBox.querySelector(".reply-input").value;
+      if (replyContent.trim() !== "") {
+        addReply(replyContent, replyElement);
+        replyBox.style.display = "none";
+        replyBox.querySelector(".reply-input").value = ""; // Clear the input field
+      }
     }
-
-    /* Add event listener for post reply button */
+  
+    // Add event listener for post reply button
     postReplyButton.addEventListener("click", postReply);
-
+  
     if (parentReply) {
-        const repliesContainer = parentReply.querySelector(".replies");
-        repliesContainer.appendChild(replyElement);
+      const repliesContainer = parentReply.querySelector(".replies");
+      repliesContainer.appendChild(replyElement);
     } else {
-        const thread = document.getElementById("comments");
-        thread.appendChild(replyElement);
+      const thread = document.getElementById("comments");
+      thread.appendChild(replyElement);
     }
-
-    /* Function to handle deleting a reply */
+  
+    // Function to handle deleting a reply
     function deleteReply() {
-        replyElement.remove();
+      replyElement.remove();
     }
-
-    /* Add event listener for delete button */
+  
+    // Add event listener for delete button
     const deleteButton = replyElement.querySelector(".delete");
     deleteButton.addEventListener("click", deleteReply);
-
-    /* Function to handle liking a reply */
+  
+    // Function to handle liking a reply
     function likeReply() {
-        const likesElement = replyElement.querySelector(".likes");
-        let likes = parseInt(likesElement.innerText);
-        likes++;
-        likesElement.innerText = likes.toString();
+      const likesElement = replyElement.querySelector(".likes");
+      let likes = parseInt(likesElement.innerText);
+      likes++;
+      likesElement.innerText = likes.toString();
     }
-
-    /* Add event listener for like button */
+  
+    // Add event listener for like button
     const likeButton = replyElement.querySelector(".like");
     likeButton.addEventListener("click", likeReply);
-
-    /* Function to handle toggling reply input visibility */
-    function toggleReplyBox() {
-        if (replyBox.style.display === "none") {
-            replyBox.style.display = "block";
-        } else {
-            replyBox.style.display = "none";
-        }
-    }
-
-    /*Add event listener for reply button */
-    const replyButton = replyElement.querySelector(".reply");
-    replyButton.addEventListener("click", toggleReplyBox);
-}
+  }
+  
